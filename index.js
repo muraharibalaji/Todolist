fe628c6896d0f048b94c5f2a78a4badc5e9e3a0c
muraharibalaji <balajisri1245@gmail.com>
2023-06-27 10:57:33 +0530

    modify commit2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const mongoose = require("mongoose");
-const dburl="mongodb+srv://balajisri1245:SB120997@cluster0.cou5got.mongodb.net/?retryWrites=true&w=majority"
+const dburl="mongodb+srv://balajisri1245:SB120997@cluster0.cou5got.mongodb.net/user?retryWrites=true&w=majority"
 mongoose.connect(dburl)
 .then((result)=>{
     console.log("DB CONNECT")
