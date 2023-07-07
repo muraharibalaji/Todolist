585cf1530189d52a133acc21cceaf2efb58285b0
muraharibalaji <balajisri1245@gmail.com>
2023-07-07 11:26:54 +0530

    multer adding

--- models/imagemodel.js
+++ models/imagemodel.js
@@ -3,11 +3,8 @@ const mongoose = require("mongoose");
 const s = mongoose.Schema
 const is = new s
 ({
-image:{
     data:Buffer,
-    contentType:String
-} 
-
-})
+    contentType:String 
+});
 const imgmodel = mongoose.model("img",is);
 module.exports = imgmodel;
