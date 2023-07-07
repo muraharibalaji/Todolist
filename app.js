2a5ad2261d5c82b7158f9ddc24289aee13bb3718
muraharibalaji <balajisri1245@gmail.com>
2023-07-07 15:42:50 +0530

    todo commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const userroutes = require('./routes/userroutes');
 const todoroutes = require('./routes/todoroutes');
-const blogroutes = require('./routes/blogroutes');
+
 
 const bodyparser = require("body-parser");
 
@@ -10,5 +10,5 @@ const app = express();
 app.use(bodyparser.json());
 app.use(userroutes);
 app.use(todoroutes);
-app.use(blogroutes);
+
 module.exports = app;
